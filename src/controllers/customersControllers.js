19cd55faabb52a3384bc230c978f67a371c183ac
Abner <abnerdasilvatavares@gmail.com>
2022-05-30 14:45:14 -0300

    Last commit

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -75,8 +75,12 @@ export async function updateCustomer(req, res) {
     }
     try {
         const exist = await db.query("SELECT * FROM customers WHERE id = $1", [id]);
-        if (!exist) {
+        if (!exist.rows[0]) {
             return res.sendStatus(404);
+        }
+        const cpfExist = await db.query(`SELECT * FROM customers WHERE cpf = $1`, [body.cpf]);
+        if (cpfExist.rows[0]) {
+            return res.sendStatus(400);
         }
         await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5`, [body.name, body.phone, body.cpf, body.birthday, id]);
         res.sendStatus(200);
